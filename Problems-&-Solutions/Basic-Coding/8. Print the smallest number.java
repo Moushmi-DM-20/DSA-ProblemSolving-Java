e037075830fd9e8779011d06d6ae66ee303f2f71
MOUSHMI D M <146321446+Moushmi-DM-20@users.noreply.github.com>
2025-06-27 16:37:46 +0530

    Create 8. Print the smallest number.java

---
+++ Problems-&-Solutions/Basic-Coding/8. Print the smallest number.java
@@ -0,0 +1,17 @@
+//Print the smallest number
+
+//solution
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter first Number : ");
+        int a = scan.nextInt();
+        System.out.print("Enter second Number : ");
+        int b = scan.nextInt();
+        System.out.print("Enter third Number : ");
+        int c = scan.nextInt();
+    int min = Math.min(c, Math.min(a,b));
+        System.out.println("Minimum Number : "+ min);    
+    }
+}
