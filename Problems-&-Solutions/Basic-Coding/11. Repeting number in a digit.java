ddc52ff4c1bc024c790c633435ee7755ac6cbdc2
MOUSHMI D M <146321446+Moushmi-DM-20@users.noreply.github.com>
2025-06-30 17:26:58 +0530

    Create 11. Repeting number in a digit.java

---
+++ Problems-&-Solutions/Basic-Coding/11. Repeting number in a digit.java
@@ -0,0 +1,19 @@
+//Print the repeting number in a digit.
+
+//Solutions
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a Number : ");
+        int num = scan.nextInt();
+        int first = 0;
+        int second = 1;
+        for(int i=0;i<=num;i++){
+            int next = first+second;
+            System.out.print(next+" ");
+            first = second;
+            second = next;
+        }
+    }
+}
