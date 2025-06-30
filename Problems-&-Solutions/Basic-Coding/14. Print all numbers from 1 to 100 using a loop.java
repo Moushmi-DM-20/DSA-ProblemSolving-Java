b50588c1e58f810e048b7f989302dcc9732c6ae7
MOUSHMI D M <146321446+Moushmi-DM-20@users.noreply.github.com>
2025-06-30 17:36:04 +0530

    Create 14. Print all numbers from 1 to 100 using a loop.java

---
+++ Problems-&-Solutions/Basic-Coding/14. Print all numbers from 1 to 100 using a loop.java
@@ -0,0 +1,10 @@
+//Print all numbers from 1 to 100 using a loop.
+
+//Solution
+class Main {
+    public static void main(String[] args) {
+        for(int i=1;i<=100;i++){
+            System.out.print(i+" ");
+        }
+    }
+}
