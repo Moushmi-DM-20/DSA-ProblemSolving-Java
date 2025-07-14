df54379e588dc32791db0af098a5d1e8db329316
MOUSHMI D M <146321446+Moushmi-DM-20@users.noreply.github.com>
2025-07-14 12:04:42 +0530

    Update 25. Find Simple Interest.java

--- Problems-&-Solutions/Basic-Coding/25. Find Simple Interest.java
+++ Problems-&-Solutions/Basic-Coding/25. Find Simple Interest.java
@@ -5,8 +5,11 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the principle amount : ");
         double principle = scan.nextDouble();
+        System.out.println("Enter the rate : ");
         int rate = scan.nextInt();
+        System.out.println("Enter the time : ");
         double time = scan.nextDouble();
         
         int SI = (int)(principle*rate*time)/100;
