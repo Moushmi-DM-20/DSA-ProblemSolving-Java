831ff1ca53e03e90f9b17c5644d79d3026234e95
MOUSHMI D M <146321446+Moushmi-DM-20@users.noreply.github.com>
2025-07-14 12:03:55 +0530

    Update 24. Print a greeting message.java

--- Problems-&-Solutions/Basic-Coding/24. Print a greeting message.java
+++ Problems-&-Solutions/Basic-Coding/24. Print a greeting message.java
@@ -5,6 +5,7 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a name : ");
         String name = scan.nextLine();
         System.out.println("Good Morning "+name+" !" );
     }
