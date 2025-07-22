dbc72b00567d0bb882ca83d40e286130a9532f1c
MOUSHMI D M <146321446+Moushmi-DM-20@users.noreply.github.com>
2025-07-22 12:56:14 +0530

    Create 02. Area of Triangle.java

---
+++ Problems-&-Solutions/Formula Related Problems/02. Area of Triangle.java
@@ -0,0 +1,15 @@
+//Area Of Triangle
+
+//Solution
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter length of the triangle :");
+        int l = scan.nextInt();
+        System.out.print("Enter breadth of the triangle :");
+        int b = scan.nextInt();
+        double area = 0.5*l*b;
+        System.out.println("Area of a triangle : "+area);
+    }
+}
