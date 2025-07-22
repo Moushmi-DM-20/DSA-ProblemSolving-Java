b563ba442ed0db55e2d2f03dc70bbfa6260c890c
MOUSHMI D M <146321446+Moushmi-DM-20@users.noreply.github.com>
2025-07-22 12:57:35 +0530

    Create 03. Area Of Rectangle.java

---
+++ Problems-&-Solutions/Formula Related Problems/03. Area Of Rectangle.java
@@ -0,0 +1,15 @@
+//Area Of Rectangle Program
+
+//Solution
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter length of the rectangle :");
+        int l = scan.nextInt();
+        System.out.print("Enter breadth of the rectangle :");
+        int b = scan.nextInt();
+        double area = l*b;
+        System.out.println("Area of a rectangle : "+area);
+    }
+}
