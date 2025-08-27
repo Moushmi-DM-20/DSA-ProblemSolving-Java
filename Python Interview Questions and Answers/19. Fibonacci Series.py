dfedd48a922ceca2747e37734b833f7b17649692
MOUSHMI D M <146321446+Moushmi-DM-20@users.noreply.github.com>
2025-08-27 12:17:56 +0530

    Create 19. Fibonacci Series.py

---
+++ Python Interview Questions and Answers/19. Fibonacci Series.py
@@ -0,0 +1,36 @@
+#Fibonacci Series
+
+#Solution
+
+#Recursion
+def fibo(n):
+    if n==0 or n==1:
+        return n
+    return fibo(n-1)+fibo(n-2)
+
+def printFibo(n):
+    for i in range(n):
+        print(fibo(i),end=" ")
+    
+inputVal = int(input("Enter a number : "))
+printFibo(inputVal)
+
+#Looping
+def fibo(n):
+    a = 0
+    b = 1
+    if n==1 or n==0:
+        return n
+    else:
+        for i in range(1,n):
+           c=a+b
+           a = b
+           b = c
+        return c
+        
+def printFibo(n):
+    for i in range(n):
+        print(fibo(i),end=" ")
+
+inputVal = int(input("Enter a number : "))
+printFibo(inputVal)
