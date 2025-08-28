45fc54019b29ef0c10ea7bd7d2afe164f7793295
MOUSHMI D M <146321446+Moushmi-DM-20@users.noreply.github.com>
2025-08-28 11:27:30 +0530

    Update 60. Program to extract N max elements.py

--- Python Interview Questions and Answers/60. Program to extract N max elements.py
+++ Python Interview Questions and Answers/60. Program to extract N max elements.py
@@ -1,10 +1,26 @@
 #Write a python program to extract N maximum elements
 
 #Solution
+
+#Using sorting
 def findNMax(arr,n):
     A1 = arr[:]
     A1.sort(reverse=True)
     return A1[:n]
+    
+inputArr= list(map(int,input("Enter numbers with spaces : ").split()))
+n = int(input("Enter a number : "))
+print(findNMax(inputArr,n))
+
+#Without Sorting
+def findNMax(arr,n):
+    A1 = arr[:]
+    result = []
+    for i in range(n):
+        maxVal = max(A1)
+        result.append(maxVal)
+        A1.remove(maxVal)
+    return result
     
 inputArr= list(map(int,input("Enter numbers with spaces : ").split()))
 n = int(input("Enter a number : "))
