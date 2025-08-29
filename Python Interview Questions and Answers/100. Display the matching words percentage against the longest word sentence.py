cc4fe1f729f3721b839b649b99735a68418b9c1f
MOUSHMI D M <146321446+Moushmi-DM-20@users.noreply.github.com>
2025-08-29 10:37:06 +0530

    Update and rename 100. Find uncommom words from two strings.py to 100. Display the matching words percentage against the longest word sentence.py

--- Python Interview Questions and Answers/100. Display the matching words percentage against the longest word sentence.py
+++ Python Interview Questions and Answers/100. Display the matching words percentage against the longest word sentence.py
@@ -1,4 +1,4 @@
-#Find uncommon words from two strings
+#Write a python program to display the matching words percentage against the longest word sentence
 
 #Solution
 def sSplit(s):
@@ -24,11 +24,21 @@ def unCommom(s1,s2):
         count[w] = count.get(w,0)+1
     return count
     
+def checkPer(s1,s2):
+    str1 = sSplit(s1)
+    str2 = sSplit(s2)
+    data = unCommom(s1,s2)
+    count = 0
+    for w, c in data.items():
+        if c>1:
+            count += 1
+    maxCount = 0
+    if len(str1)>len(str2):
+        maxCount = len(str1)
+    else : 
+        maxCount = len(str2)
+    return (count/maxCount)*100
+    
 fInput = input("Enter a string : ")
 sInput = input("Enter a string : ")
-result = unCommom(fInput,sInput)
-output = []
-for w, count in result.items():
-    if count==1:
-        output.append(w)
-print(output)
+print(f"{checkPer(fInput,sInput):.2f}%")
