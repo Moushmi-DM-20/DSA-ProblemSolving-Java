cf79ba6925afe0211f99b0dbea1eae9e52178e46
MOUSHMI D M <146321446+Moushmi-DM-20@users.noreply.github.com>
2025-09-01 23:57:11 +0530

    Create 004. Sort an array.py

---
+++ Python Interview Questions and Answers/004. Sort an array.py
@@ -0,0 +1,29 @@
+#Sort an array 
+
+#Ascending Order
+
+#Solution 1
+arr = [4,9,1,3,8,5]
+
+result = sorted(arr)
+print(result)
+
+#Solution 2
+arr = [4,9,1,3,8,5]
+
+arr.sort()
+print(arr)
+
+#Descending Order
+
+#Solution 1
+arr = [4,9,1,3,8,5]
+
+result = sorted(arr, reverse=True)
+print(result)
+
+#Solution 2
+arr = [4,9,1,3,8,5]
+
+arr.sort(reverse = True)
+print(arr)
