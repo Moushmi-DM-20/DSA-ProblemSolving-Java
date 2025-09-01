4bec6e298c91730567c386dc34763f0192a725bf
MOUSHMI D M <146321446+Moushmi-DM-20@users.noreply.github.com>
2025-09-01 20:23:12 +0530

    Create 120. Hexadecimal to decimal.py

---
+++ Python Interview Questions and Answers/120. Hexadecimal to decimal.py
@@ -0,0 +1,49 @@
+#Hexadecimal to decimal
+
+#Solution
+def hexaValues(c):
+    if c=='0':
+        return 0
+    if c=='1':
+        return 1
+    if c=='2':
+        return 2
+    if c=='3':
+        return 3
+    if c=='4':
+        return 4
+    if c=='5':
+        return 5
+    if c=='6':
+        return 6
+    if c=='7':
+        return 7
+    if c=='8':
+        return 8
+    if c=='9':
+        return 9
+    if c=='A' or c =='a':
+        return 10
+    if c=='B' or c =='b':
+        return 11
+    if c=='C' or c =='c':
+        return 12
+    if c=='D' or c =='d':
+        return 13
+    if c=='E' or c =='e':
+        return 14
+    if c=='F' or c =='f':
+        return 15
+        
+def hexaDecimal(s):
+    digit = []
+    for c in s:
+        digit.append(hexaValues(c))
+    deci,power = 0,0
+    for i in digit[::-1]:
+        deci += i*(16**power)
+        power += 1
+    return deci
+    
+s = input("Enter : ")
+print(hexaDecimal(s))
