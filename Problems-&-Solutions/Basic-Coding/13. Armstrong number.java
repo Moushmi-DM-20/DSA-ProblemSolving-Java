a4e0f58ff9798c8b1333591fc4efa22043e090b8
MOUSHMI D M <146321446+Moushmi-DM-20@users.noreply.github.com>
2025-10-24 17:50:39 +0530

    Update 13. Armstrong number.java

--- Problems-&-Solutions/Basic-Coding/13. Armstrong number.java
+++ Problems-&-Solutions/Basic-Coding/13. Armstrong number.java
@@ -7,15 +7,25 @@ class Main {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a number : ");
         int a = scan.nextInt();
-        boolean result = checkArmstrong(a);
+        int count = checkCount(a);
+        boolean result = checkArmstrong(a,count);
         System.out.println("The given number is Armstrong Number - "+ result);
     }
-    static boolean checkArmstrong(int a){
+    static int checkCount(int a){
+        int count = 0;
+        while(a>0){
+            int rem = a%10;
+            count ++;
+            a = a/10;
+        }
+        return count;
+    }
+    static boolean checkArmstrong(int a,int count){
         int original = a;
         int sum = 0;
         while(a>0){
             int rem = a%10;
-            sum = sum+(rem*rem*rem);
+            sum += Math.pow(rem,count);
             a = a/10;    
         }
         return sum == original;
